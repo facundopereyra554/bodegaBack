dc54edac12f9a0fad726b94c32d783044e82887e
facundope <facundopereyra5566@gmail.com>
2026-06-14 13:21:32 -0300

    Optimizar rendimiento de base de datos

--- database.py
+++ database.py
@@ -3,11 +3,11 @@ from sqlmodel import create_engine, SQLModel
 
 # Base de datos de catálogo y productos
 DATABASE_URL = "sqlite:///tienda.db"
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL, echo=False, connect_args={"check_same_thread": False, "timeout": 15})
 
 # Base de datos independiente para el registro de compras
 COMPRAS_DATABASE_URL = "sqlite:///compras.db"
-engine_compras = create_engine(COMPRAS_DATABASE_URL, echo=True)
+engine_compras = create_engine(COMPRAS_DATABASE_URL, echo=False, connect_args={"check_same_thread": False, "timeout": 15})
 
 def create_db_and_tables():
     import models  # Nos aseguramos de registrar los modelos en la metadata
